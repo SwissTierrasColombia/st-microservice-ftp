cdd491c7ebdf257c936ff4f2ebe4ed42f0ca6e5b
shiido <jhonfr29@gmail.com>
2020-07-21 17:20:38 -0500

    feat: Start project

---
+++ src/main/java/com/ai/st/microservice/ftp/StMicroserviceFtpApplication.java
@@ -0,0 +1,13 @@
+package com.ai.st.microservice.ftp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StMicroserviceFtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StMicroserviceFtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/st/microservice/ftp/StMicroserviceFtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.st.microservice.ftp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StMicroserviceFtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
